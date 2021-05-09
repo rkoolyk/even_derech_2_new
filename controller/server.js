26236aad25fc0ddae684eccb4a9465203b8a42da
rkoolyk <rachelkoolyk@gmail.com>
2021-05-09 22:02:44 +0300

    Initial commit

---
+++ controller/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const fileUpload = require('express -fileupload')
+//const Model = require('../model/')
+const app = express()
+
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(fileUpload())
+app.use(express.static('../view'))
+
+app.get("/", (req, res)=>{
+    res.sendFile("./index.html")
+})
+
+
+
+
+app.listen(8080)
