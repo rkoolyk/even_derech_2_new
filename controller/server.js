b179de07b1f9539c751dcdfedc3cb9376b24b82f
tashmanr <rebeccatashman@gmail.com>
2021-05-23 12:59:27 +0300

    changed /upload to /detect

--- controller/server.js
+++ controller/server.js
@@ -4,61 +4,74 @@ const fileUpload = require('express-fileupload');
 const simpleDetect = require('../model/SimpleAnomalyDetector');
 const circleDetect = require('../model/CircleAnomalyDetector');
 const timeSeries = require('../model/timeSeries');
-//const fileUpload = require('../view/index');
-
+//const fs = require('fs');
 
 const app = express();
 app.use(express.urlencoded({
-    extended: false
+    extended: true
 }));
+
 app.use(express.static('../view'));
 
 app.use(fileUpload());
 
-app.use(express.json());
-
-const current = new Date();
-//let num = 0;
-
-/*const models = [
-    { model_id: 1, upload_time: '8:00', status: 'ready' },
-    { model_id: 2, upload_time: '10:00', status: 'pending' }
-];*/
-
-app.post('/upload', (req, res) => {
+app.post('/detect', (req, res) => {
     console.log('Uploading...');
     let flightCSV;
     let trainCSV;
     let pathFlight;
     let pathTrain;
+    let algorithm;
 
-    if (!req.files || Object.keys(req.files).length === 0) return res.status(400).send('No files were uploaded.'); // 400 - bad request
+    //if (!req.files || Object.keys(req.files).length === 0) return res.status(400).send('No files were uploaded.'); // 400 - bad request
 
-    flightCSV = req.files.flightCSV;
-    trainCSV = req.files.trainCSV;
-    pathFlight = __dirname + '/uploads/' + flightCSV.name;
-    pathTrain = __dirname + '/uploads/' + trainCSV.name;
+    //flightCSV = req.files.flightCSV;
+    //trainCSV = req.files.trainCSV;
+    //pathFlight = __dirname + '/uploads/' + flightCSV.name;
+    //pathTrain = __dirname + '/uploads/' + trainCSV.name;
+    pathFlight = '../testFile.csv'
+    trainFlight = '../trainFile.csv'
+    if (req.body.chosenAlgorithm === 'Hybrid Alorithm') algorithm = circleDetect;
+    else algorithm = simpleDetect;
 
-    trainCSV.mv(pathTrain, function (err) {
+    console.log('Training file uploaded!');
+    const trainTS = timeSeries.Timseries(pathTrain);
+    console.log('Training time series uploaded!');
+    algorithm.learnNormal(trainTS);
+    console.log('Learning normal completed!');
+
+    /*trainCSV.mv(pathTrain, function (err) {
         if (err) return res.status(500).send(err); // 500 - upload error
         console.log('Training file uploaded!');
         const trainTS = timeSeries.Timseries(pathTrain);
         console.log('Training time series uploaded!');
-        simpleDetect.learnNormal(trainTS);
+        algorithm.learnNormal(trainTS);
         console.log('Learning normal completed!');
-    });
+    });*/
 
-    flightCSV.mv(pathFlight, function (err) {
+    console.log('Flight file uploaded!');
+    const flightTS = timeSeries.Timseries(pathFlight);
+    console.log('Flight time series uploaded!');
+    const result = algorithm.detect(flightTS);
+    console.log('Detection completed!');
+    res.status(200).send(result).sendFile("./index.html"); // 200 - success
+
+    /*flightCSV.mv(pathFlight, function (err) {
         if (err) return res.status(500).send(err); // 500 - upload error
-        res.send('Flight file uploaded!');
-    });
+        console.log('Flight file uploaded!');
+        const flightTS = timeSeries.Timseries(pathFlight);
+        console.log('Flight time series uploaded!');
+        const result = algorithm.detect(flightTS);
+        console.log('Detection completed!');
+        res.status(200).send(result).sendFile("./index.html"); // 200 - success
+    });*/
 
     
 });
 
 
 app.get('/', (req, res) => {
-    res.sendFile("./index.html")
+    res.sendFile("./index.html");
 });
 
 /*app.post('/api/model/:model_type', (req, res) => {
@@ -113,9 +126,5 @@ app.post('/api/anomaly/:model_id', (req, res) => {
     res.send(modelRequested);
 });*/
 
-
-
-
-
-const port = 8080;
+const port = 8080; 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
