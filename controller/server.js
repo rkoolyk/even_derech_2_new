e4efa097aebce1eb5d71891971086f258c001048
tashmanr <rebeccatashman@gmail.com>
2021-05-25 17:54:19 +0300

    removed use of Joi

--- controller/server.js
+++ controller/server.js
@@ -1,8 +1,7 @@
-const Joi = require('joi');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const simpleDetect = require('../model/SimpleAnomalyDetector');
-const circleDetect = require('../model/HybridAnomalyDetector');
+const hybridDetect = require('../model/HybridAnomalyDetector');
 const timeSeries = require('../model/timeSeries');
 
 const app = express();
@@ -24,7 +23,7 @@ app.post('/detect', (req, res) => {
 
     flightCSV = req.files.flightCSV;
     trainCSV = req.files.trainCSV;
-    if (req.body.chosenAlgorithm === 'Hybrid Algorithm') algorithm = circleDetect;
+    if (req.body.chosenAlgorithm === 'Hybrid Algorithm') algorithm = hybridDetect;
     else algorithm = simpleDetect;
     console.log('Training file uploaded!');
     let trainTS = new timeSeries.Timseries(trainCSV);
@@ -44,57 +43,6 @@ app.post('/detect', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile("./index.html");
 });
-/*app.post('/api/model/:model_type', (req, res) => {
-    const schema = {
-        train_data: Joi.required()
-    };
-
-    const result = Joi.validate(req.body, schema);
-
-    if (result.error) return res.status(400).send(result.error.details[0].message); // 400 - bad request
-
-    const model = {
-        model_id: this.num + 1,
-        upload_time: current.getTime(),
-        status: 'pending'
-    };
-
-    models.push(model);
-    num = num + 1;
-    res.status(200); // 200 - success
-    res.send(model);
-});
-
-app.get('/api/model/:model_id', (req, res) => {
-    //check if model exists
-    const modelRequested = models.find(c => c.model_id === parseInt(req.params.model_id));
-    if (!modelRequested) return res.status(404).send(`Model with ID: ${req.params.model_id} was not found.`); // 404 - not found
-    //return the model
-    res.send(modelRequested);
-});
-
-app.delete('/api/model/:model_id', (req, res) => {
-    //check if model exists
-    const modelRequested = models.find(c => c.model_id === parseInt(req.params.model_id));
-    if (!modelRequested) return res.status(404).send(`Model with ID: ${req.params.model_id} was not found.`); // 404 - not found
-
-    // delete the course
-    const index = models.indexOf(modelRequested);
-    models.splice(index, 1);
-
-    // return the model that was deleted
-    res.send(modelRequested);
-});
-
-app.get('/api/models', (req, res) => {
-    res.send(models);
-});
-
-app.post('/api/anomaly/:model_id', (req, res) => {
-    const modelRequested = models.find(c => c.model_id === parseInt(req.params.model_id));
-    if (!modelRequested) return res.status(404).send(`Model with ID: ${req.params.model_id} was not found.`); // 404 - not found
-    res.send(modelRequested);
-});*/
 
 const port = 8080; 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
